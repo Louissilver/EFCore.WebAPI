2159edf3608f822e6cb16e4759e0017f94e8d3f2
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-06-02 18:29:22 -0300

    Mudança de Models para Class Library EFCore.Dominio e Data para Class Library EFCore.Repo

--- EFCore.Repo/HeroiContext.cs
+++ EFCore.Repo/HeroiContext.cs
@@ -1,23 +1,21 @@
-﻿using EFCore.WebAPI.Models;
+﻿using EFCore.Dominio;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EFCore.WebAPI.Data
+namespace EFCore.Repo
 {
     public class HeroiContext : DbContext
     {
+        public HeroiContext(DbContextOptions<HeroiContext> options) : base(options) { }
         public DbSet<Heroi> Herois { get; set; }
         public DbSet<Arma> Armas { get; set; }
         public DbSet<Batalha> Batalhas { get; set; }
         public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
         public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-HOSO0DN");
-        }
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
